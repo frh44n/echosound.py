39ad6c12934afa10bb94eef1f25a0ddb4c44f781
frh44n <107492548+frh44n@users.noreply.github.com>
2024-06-07 03:47:55 +0530

    Update echo.py

--- echo.py
+++ echo.py
@@ -4,7 +4,7 @@ import os
 
 # Bot token and webhook URL
 TOKEN = '6529945909:AAEj6Exy95DuR5_J72_D3ht2DUzrvTGzOfQ'
-WEBHOOK_URL = 'https://echosound-py-1.onrender.com'
+WEBHOOK_URL = 'https://echosound-py-2.onrender.com'
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text('Hello! I am an echo bot. Send me any message and I will echo it back.')
